b151436102faba0933ab071a68494ba360e6648c
ChiWoo-Shin <shin8037@naver.com>
2022-10-06 21:57:04 +0900

    풀었으나 답안지를 참고하였음..

--- B1991/B1991_scw.py
+++ B1991/B1991_scw.py
@@ -1,1 +1,35 @@
-#scw_test
+import sys
+
+input=sys.stdin.readline
+
+N=int(input())
+
+A={}
+for i in range(N):
+    root,left,right=input().strip().split()
+    A[root]=[left,right]
+
+def preorder(root):
+    if root != ".":
+        print(root,end="")
+        preorder(A[root][0])
+        preorder(A[root][1])
+
+def inorder(root):
+    if root !=".":
+        inorder(A[root][0])
+        print(root,end="")        
+        inorder(A[root][1])
+
+def postorder(root):
+    if root !=".":
+        postorder(A[root][0])        
+        postorder(A[root][1])
+        print(root,end="")
+
+
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
