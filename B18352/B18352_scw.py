b0734789eb1ccb5cd5aafa18de46e331974c452d
ChiWoo-Shin <shin8037@naver.com>
2022-10-08 23:02:38 +0900

    122080kb 2424ms

---
+++ B18352/B18352_scw.py
@@ -0,0 +1,45 @@
+import sys
+from collections import defaultdict, deque
+sys.setrecursionlimit(10**8)
+
+input=sys.stdin.readline
+
+
+N,M,K,X = map(int,input().strip().split())
+
+city=defaultdict(list)
+for i in range(M):
+    a,b=map(int,input().strip().split())
+    city[a].append(b)
+    #city[b].append(a)
+
+# print(city)
+visited=[False]*(N+1)
+cnt=0
+answer_list=[]
+def bfs(X,cnt,K):
+    dq=deque()
+    dq.append((X,cnt))
+    visited[X]=True
+    while dq:
+        a,cnt=dq.popleft()
+        if cnt==K:
+            answer_list.append(a)
+    
+        for i in city[a]:
+            if visited[i]==False:
+                dq.append((i,cnt+1))
+                visited[i]=True
+
+bfs(X,cnt,K)
+
+
+visited[X]=0
+# print(answer_list)
+# print(visited)
+    
+if len(answer_list)==0:
+    print(-1)
+else:
+    answer_list.sort()
+    print(*answer_list,sep='\n')
